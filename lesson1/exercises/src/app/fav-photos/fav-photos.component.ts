41e9dcc981651833ff7d287552002c0a0425a3cd
Ella Vankirk <ella.vankirk@gmail.com>
2020-11-02 17:10:32 -0600

    Part 1 of exercise

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'These are a few of my favorite things:';
+  image1 = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.CLgIfaB8qchOEgdTgph0PgHaEK%26pid%3DApi&f=1';
+  image2 = 'https://preview.redd.it/fc94t2ru5vw51.jpg?width=640&crop=smart&auto=webp&s=55f2f1c7a1988c02a180fdfccaf6a2b816236966';
+  image3 = 'https://source.colostate.edu/wp-content/uploads/2016/03/wild.horses.slideshow.7.original.jpg';
 
   constructor() { }
 
